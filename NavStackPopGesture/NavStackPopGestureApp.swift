7c332725cd273da902279c716023d6747afc8de7
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2023-10-08 15:37:09 -0400

    Initial Commit

---
+++ NavStackPopGesture/NavStackPopGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackPopGestureApp.swift
+//  NavStackPopGesture
+//
+//  Created by Kartikeya Saxena Jain on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct NavStackPopGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
