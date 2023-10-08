22927858e426f51424999b5ebf60fe90ce6b5ce6
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2023-10-08 19:55:16 -0400

    Final

--- NavStackPopGesture/ContentView.swift
+++ NavStackPopGesture/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isEnabled: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        FullSwipeNavigationStack {
+            List {
+                Section("Sample Header") {
+                    NavigationLink("Full Swipe View") {
+                        List {
+                            Toggle("Enable Full Swipe Pop", isOn: $isEnabled)
+                                .enableFullSwipePop(isEnabled)
+                        }
+                        .navigationTitle("Full Swipe View")
+                    }
+                    
+                    NavigationLink("Leading Swipe View") {
+                        Text("")
+                            .navigationTitle("Leading Swipe View")
+                    }
+                }
+            }
+            .navigationTitle("Full Swipe Pop")
         }
-        .padding()
     }
 }
 
